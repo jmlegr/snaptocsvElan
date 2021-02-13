bd94e3e65fb97afe7f905d1613524a6bad5adc28
duff <jmlegr@gmail.com>
2021-02-13 09:46:14 +0100

    prise en compte VAL (hors script et instruction)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dbName = 'sierpinski_db';
 const coll = 'reconstructions';
 const {parse} = require('json2csv');
 const fs = require('fs');
+const h2p = require('html2plaintext');
 var readlineSync = require('readline-sync');
 //const fields = ['field1', 'field2', 'field3'];
 //const opts = { fields };
@@ -78,6 +79,30 @@ async function run() {
                 '$project': {'commandes':0}
             }
         ];
+        const aggVal=[
+            {
+                '$match':{'session_key': 'iptrff5x17betctynr3xuwi19duaphy2','commandes.evt.type':/VAL/}
+            }, {
+                '$addFields': {
+                    'temps_adjust': {
+                        '$add': [
+                            '$commandes.temps', offset_temps
+                        ]
+                    },
+                    'truc': {
+                        '$concat': [
+                            '$commandes.epr.type', '--', '$commandes.epr.detail'
+                        ]
+                    },
+                    'acteur':'VALEURS',
+                    'annotation':'$commandes.evt.type'
+                }
+            }, {
+                '$sort':{time:1}
+            }
+
+
+        ]
         const epr_max = await collection.findOne({}, {sort: {etape: -1}});
         const t_max = epr_max.commandes.temps + offset_temps; //temps de la dernièreaction EPR (normalement SNP+SAVE)
         //console.log("MAX TERMPS", t_max)
@@ -105,7 +130,18 @@ async function run() {
         const promise2=collection.aggregate(aggENV).toArray().then(r => {
             r.forEach(c=>{
                 c.temps_fin=Math.min(c.temps_adjust+duree,t_max)
-                //console.log(c.etape,c.acteur,c.temps_adjust,c.temps_fin,c.annotation)
+            });
+            return r;
+        })
+        //traitement des changements de valeurs (sans indication de script ou d'instruction)
+        const promise3=collection.aggregate(aggVal).toArray().then(r=>{
+            r.forEach((c,i,a)=>{
+                const el=c.commandes.snap.filter(s=> (s.change!=null && s.change.match(/val_/)))
+                el.forEach(e=>{
+                    //const a=e.change.match(/.*(<<.*>>).*/); //pour ajout ancienne valeur a?a[1]:''
+                    c.annotation='('+c.annotation+'): '+h2p(e.commande);
+                    c.temps_fin=Math.min(c.temps_adjust+duree,t_max)
+                })
             });
             return r;
         })
@@ -126,6 +162,7 @@ async function run() {
         };
         promise1.then(tocsv);
         promise2.then(tocsv);
+        promise3.then(tocsv)
     } finally {
         await client.close()
 
